3da197bff3046969eb7b2533eceebe52d258ed2a
Logan Stone <logan.stone20@gmail.com>
2019-08-02 01:44:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,17 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    win_index_1 = combination[0]
+    win_index_2 = combination[1]
+    win_index_3 = combination[3]
+    if board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X"
+      return combination
+    end
+    if board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"
+      return combination
+    end
+  end
+end
